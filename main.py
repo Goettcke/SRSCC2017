31df08345e50ca3d7728672a1c84cd2f3ff69ce5
Jonatan Møller Gøttcke <jogoe12@student.sdu.dk>
2017-08-11 11:51:43 +0200

    Now made some fun

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-print "hello"
+import sys
+from Person import Person
+print "Susanne, Regina"
 
-fo = open("TOY/FT1845.csv") # This is simple how to open a file
+
+reload(sys)
+fo = open("toy/FT1845.csv") # This is simple how to open a file
+
+line = fo.readline().decode('iso-8859-1') # For at kunne haandterer danske tegn
+line = fo.readline().decode('iso-8859-1') # For at kunne haandterer danske tegn
+
+lineSplit = line.split("|")
+
+p = Person(1845)
+p.herred = lineSplit[4]
+
+print "Person year: " + str(p.year)
+print "herred " + p.herred
+print line
+
 
