539685e7cc96e00a3d33566e1f550e8685422abc
Jonatan Møller Gøttcke <jogoe12@student.sdu.dk>
2017-08-16 22:21:44 +0200

    now with double metaphone + damereau somethingson distance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import getData
+from dm import damerau_levenshtein_distance
 from metaphone import singlemetaphone
 
 from Person import Person, foedested_comparison, foedeaar_comparison
@@ -9,16 +10,38 @@ print "Susanne, Regina"
 
 
 
-f = "1845smallutf8.csv"
-getData.get_people_to(f,"k")
+#f = "1845smallutf8.csv"
+#getData.get_people_to(f,"k")
 
 
-p = getData.people[2]
-assert isinstance(p, Person)
+#p = getData.people[2]
 
-print p.fornavn + " " + p.mlnavn + " " + p.efternavn
+#assert isinstance(p, Person)
 
-metaphoneret = singlemetaphone(p.efternavn,1)
-print metaphoneret
+#print p.fornavn + " " + p.mlnavn + " " + p.efternavn
 
+p1 = "poul"
+p2 = "povl"
+
+dmres = damerau_levenshtein_distance(p1,p2)
+print dmres
+
+
+
+metaphoneretp1 = singlemetaphone(p1,1)
+metaphoneretp2 = singlemetaphone(p2,1)
+
+print metaphoneretp1
+print metaphoneretp2
+
+
+dmres = damerau_levenshtein_distance(metaphoneretp1,metaphoneretp2)
+
+
+
+
+
+
+
+print dmres
 #metaphoneret = singlemetaphone(niels laustsen pleiesøn,1)
