7e83b10cc77a1c0bed81c6b925aa2d3ae35873d0
Peter Severin Rasmussen <git@petersr.com>
2017-10-25 15:21:43 +0200

    Made slurm put the out and err into a folder

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#
 #SBATCH --account sdusscsa1_slim
 #SBATCH --nodes 1
 #SBATCH --time 23:59:00
+#SBATCH -o slurm-dump/slurm.%N.%j.out # stdout
+#SBATCH -e slurm-dump/slurm.%N.%j.err # stderr
 
 ./main.py $1 $2 4 $3
