caf6be9f2b8ce93ac29423045d801c05dd21fc54
Peter Severin Rasmussen <git@petersr.com>
2017-11-05 13:39:17 +0100

    Updated clean up script

--- clean-up.sh
+++ clean-up.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#SBATCH --account sdusscsa1_slim
+#SBATCH --nodes 1
+#SBATCH --time 23:59:00
+#SBATCH -o slurm-dump/slurm.%N.%j.out # stdout
+#SBATCH -e slurm-dump/slurm.%N.%j.err # stderr
+
 set -o xtrace
 
 rm -f *.pyc
@@ -9,3 +15,4 @@ rm -rf output*sample
 mkdir -p stash
 mv output* stash
 rm -rf stash/output*
+rm -rf stash
